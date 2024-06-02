26abb09ca84ec9396ae140485207a4575aab8653
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-02 12:42:59 -0400

    inner vibe frontend project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+      <main>
+        <div></div>
+        <h1>Hello</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>NavBar</h1>
+    </nav>
+  );
+};
+
+export default NavBar;
