a7a035d820c299af65ac58cd1e3e6f6f85372a61
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-02 21:03:52 -0400

    add authentication and routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
       <main>
-        <div></div>
-        <h1>Hello</h1>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
       </main>
     </>
   );
