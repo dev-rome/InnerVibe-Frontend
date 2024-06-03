14d2738566d93973b4e961e8d4cbae5cdd190b2d
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-03 10:36:01 -0400

    add react suspense and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 import PrivateRoute from "./components/PrivateRoute";
+
+// Lazy load the components
+const Login = lazy(() => import("./pages/Login/Login"));
+const Signup = lazy(() => import("./pages/Signup/Signup"));
+const Home = lazy(() => import("./pages/Home/Home"));
 
 function App() {
   return (
     <>
-      <main>
-        <AuthProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="/" element={<Home />} />
-            </Route>
-          </Routes>
-        </AuthProvider>
-      </main>
+      <AuthProvider>
+        <main>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/" element={<Home />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </main>
+      </AuthProvider>
     </>
   );
 }
