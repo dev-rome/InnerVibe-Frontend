144dca757ff8d6c08112a50b30032cef50140d12
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-02 21:00:36 -0400

    add message state and fix type error

--- src/components/EntryInput.tsx
+++ src/components/EntryInput.tsx
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import AuthContext from "../context/AuthContext";
 
-const MoodInput = () => {
+const EntryInput = () => {
   const [entry, setEntry] = useState("");
   const [title, setTitle] = useState("");
+  const [message, setMessage] = useState<string | null>(null);
+
+  const token = useContext(AuthContext);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const url = "http://localhost:5001/api/entry";
     try {
+      const headers: HeadersInit = {
+        "Content-Type": "application/json",
+      };
+      if (token) {
+        headers["x-auth-token"] = String(token);
+      }
+
       const res = await fetch(url, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers,
         body: JSON.stringify({ entry, title }),
       });
       const data = await res.json();
+      setMessage(data.message);
       console.log("Response", data);
     } catch (err) {
       console.error("Error submitting mood:", err);
+      setMessage("Error submitting entry");
     }
     setEntry("");
     setTitle("");
@@ -40,8 +51,9 @@ const MoodInput = () => {
         <textarea value={entry} onChange={(e) => setEntry(e.target.value)} />
       </label>
       <button>Submit</button>
+      {message && <p>{message}</p>}
     </form>
   );
 };
 
-export default MoodInput;
+export default EntryInput;
